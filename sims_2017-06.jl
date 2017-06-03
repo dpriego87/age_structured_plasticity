2e7e5023dab85dcf77aec5641a4448d3e65fb5c7
Jeremy Van Cleve <jeremy.vancleve@gmail.com>
2017-06-03 17:56:16 -0400

    update tests

--- sims_2017-06.jl
+++ sims_2017-06.jl
@@ -11,11 +11,15 @@ iters = 1000
 pops = Array(Population, length(γb_vec))
 mgs = Array(Any, length(γb_vec))
 
+srand(3141);
 for i = 1:length(γb_vec)
     println("iter ", i)
-    pops[i] = popLinearPlasticityFert(n=500, s=[0.9, 0.9, 0.1, 0.0], ve=0.01,
-                                      A=1.0, B=1.0, γ=2.0, γb=γb_vec[i], wmax=3.0,
-                                      venv=0.1, arθ=0.75, vmut=0.01);
+    # pops[i] = popLinearPlasticityFert(n=500, s=[0.9, 0.9, 0.1, 0.0], ve=0.01,
+    #                                   A=1.0, B=1.0, γ=2.0, γb=γb_vec[i], wmax=3.0,
+    #                                   venv=0.1, arθ=0.75, vmut=0.01);
+    pops[i] = linearPlasticityFertAgePop(500, [0.9, 0.9, 0.1, 0.0], 0.01,
+                                         1.0, 1.0, 2.0, γb_vec[i], 3.0,
+                                         0.1, 0.75, 0.01);
     @time (mg, env) = runSim(pops[i], reps, burns, iters);
     mgs[i] = mg
 end
@@ -23,11 +27,7 @@ end
 ls = transpose(hcat(map((x)->landeSlope(1., 1., 2., x, 0.1, 0.75), γb_vec)...))
 
 ## Plot intercept for the different cost values
-clf()
-plot(ls[:,1])
-plot(hcat(map((x)->mean(x[1:2:7,:],2), mgs)...))
-
-clf()
+figure()
 subplot(221)
 plotBoxPlot(mgs[1][1:2:7,:]', landeSlope(1., 1., 2., γb_vec[1], 0.1, 0.75)[1], ["0", "1", "2", "3"], "black")
 subplot(222)
@@ -38,10 +38,7 @@ subplot(224)
 plotBoxPlot(mgs[4][1:2:7,:]', landeSlope(1., 1., 2., γb_vec[4], 0.1, 0.75)[1], ["0", "1", "2", "3"], "black")
 
 ## Plot slope for the different cost values
-clf()
-plot(ls[:,2])
-plot(hcat(map((x)->mean(x[2:2:8,:],2), mgs)...))
-
+figure()
 clf()
 subplot(221)
 plotBoxPlot(mgs[1][2:2:8,:]', landeSlope(1., 1., 2., γb_vec[1], 0.1, 0.75)[2], ["0", "1", "2", "3"], "black")
@@ -53,7 +50,8 @@ subplot(224)
 plotBoxPlot(mgs[4][2:2:8,:]', landeSlope(1., 1., 2., γb_vec[4], 0.1, 0.75)[2], ["0", "1", "2", "3"], "black")
 
 
-
+savefig("testoldA.pdf", bbox_inches="tight")
+savefig("testoldB.pdf", bbox_inches="tight")
 
 save("fert_no_cost_01.jld", "mg", mg, "env", env)
 
