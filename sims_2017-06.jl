3adb56f517af3b7b575e43acc45d74bc39b2cdd7
Jeremy Van Cleve <jeremy.vancleve@gmail.com>
2017-06-04 01:21:45 -0400

    new code seems to reproduce old results.

--- sims_2017-06.jl
+++ sims_2017-06.jl
@@ -14,12 +14,12 @@ mgs = Array(Any, length(γb_vec))
 srand(3141);
 for i = 1:length(γb_vec)
     println("iter ", i)
-    # pops[i] = popLinearPlasticityFert(n=500, s=[0.9, 0.9, 0.1, 0.0], ve=0.01,
-    #                                   A=1.0, B=1.0, γ=2.0, γb=γb_vec[i], wmax=3.0,
-    #                                   venv=0.1, arθ=0.75, vmut=0.01);
-    pops[i] = linearPlasticityFertAgePop(500, [0.9, 0.9, 0.1, 0.0], 0.01,
-                                         1.0, 1.0, 2.0, γb_vec[i], 3.0,
-                                         0.1, 0.75, 0.01);
+    pops[i] = popLinearPlasticityFert(n=500, s=[0.9, 0.9, 0.1, 0.0], ve=0.01,
+                                      A=1.0, B=1.0, γ=2.0, γb=γb_vec[i], wmax=3.0,
+                                      venv=0.1, arθ=0.75, vmut=0.01);
+    # pops[i] = linearPlasticityFertAgePop(500, [0.9, 0.9, 0.1, 0.0], 0.01,
+    #                                      1.0, 1.0, 2.0, γb_vec[i], 3.0,
+    #                                      0.1, 0.75, 0.01);
     @time (mg, env) = runSim(pops[i], reps, burns, iters);
     mgs[i] = mg
 end
@@ -50,18 +50,11 @@ subplot(224)
 plotBoxPlot(mgs[4][2:2:8,:]', landeSlope(1., 1., 2., γb_vec[4], 0.1, 0.75)[2], ["0", "1", "2", "3"], "black")
 
 
-savefig("testoldA.pdf", bbox_inches="tight")
-savefig("testoldB.pdf", bbox_inches="tight")
+savefig("testnewA.pdf", bbox_inches="tight")
+savefig("testnewB.pdf", bbox_inches="tight")
 
 save("fert_no_cost_01.jld", "mg", mg, "env", env)
-
 mg, env = load("fert_no_cost_01.jld", "mg", "env");
-clf()
-plotBoxPlot(mg[1:2:7,:]', landeSlope(A, B, γ, γb, venv, arθ)[1], ["0", "1", "2", "3"], "black")
-savefig("fert_no_cost_intercept_01.pdf", bbox_inches="tight")
-clf()
-plotBoxPlot(mg[2:2:8,:]', landeSlope(A, B, γ, γb, venv, arθ)[2], ["0", "1", "2", "3"], "black")
-savefig("fert_no_cost_slope_01.pdf", bbox_inches="tight")
 
 
 ## timing
